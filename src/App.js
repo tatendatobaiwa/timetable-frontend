1bc693114e5897a98feedf610fca7c581cf8e6a0
tatenda.tobaiwa <tatenda.tobaiwa@gmail.com>
2025-01-24 17:08:30 +0000

    push

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Home from "./pages/Home/Home";
@@ -13,26 +13,37 @@ import SignUp from "./pages/SignUp/SignUp";
 import SignIn from "./pages/SignIn/SignIn";
 import "./App.css";
 
+const hideHeaderFooterRoutes = ['/', '/signup', '/signin'];
+
+function AppContent() {
+  const location = useLocation();
+  const shouldHideHeaderFooter = hideHeaderFooterRoutes.includes(location.pathname);
+
+  return (
+    <div className="ScheduleMaster">
+      {!shouldHideHeaderFooter && <Header />}
+      <main>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/timetable-maker" element={<TimetableMaker />} />
+          <Route path="/terms-of-use" element={<TermsOfUse />} />
+          <Route path="/exam-timetable" element={<ExamTimetable />} />
+          <Route path="/teaching-timetable" element={<TeachingTimetable />} />
+          <Route path="/about-us" element={<AboutUs />} />
+        </Routes>
+      </main>
+      {!shouldHideHeaderFooter && <Footer />}
+    </div>
+  );
+}
+
 function App() {
   return (
     <Router>
-      <div className="ScheduleMaster">
-        <Header />
-        <main>
-          <Routes>
-            <Route path="/home" element={<Home />} />
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/signin" element={<SignIn />} />
-            <Route path="/timetable-maker" element={<TimetableMaker />} />
-            <Route path="/terms-of-use" element={<TermsOfUse />} />
-            <Route path="/exam-timetable" element={<ExamTimetable />} />
-            <Route path="/teaching-timetable" element={<TeachingTimetable />} />
-            <Route path="/about-us" element={<AboutUs />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
+      <AppContent />
     </Router>
   );
 }
